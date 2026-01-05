9ce3613558b1eeebe074d99031ab760c02a03027
Eric <eazyelivin@gmail.com>
2026-01-05 01:11:16 -0500

    Fix iOS Safari safe area

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import "./globals.css"
 import { CartProvider } from "./components/CartProvider"
 
+export const metadata = {
+  viewport: "width=device-width, initial-scale=1, viewport-fit=cover",
+}
+
 export default function RootLayout({
   children,
 }: {
